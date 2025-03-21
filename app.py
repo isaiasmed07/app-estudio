b3a9064f10991ecc72ee4da6434f2483de881d63
isaiasmed07 <isaiasmed684@gmail.com>
2025-03-21 14:43:40 -0600

    Configurar rutas para lecciones y Lecciones

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from flask import Flask, jsonify, request
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-# Inicializar Firebase Admin SDK
-cred = credentials.Certificate('firebase/credentials.json')
+# Cargar el archivo de credenciales desde la ruta absoluta
+cred = credentials.Certificate(r'C:\app-estudio-clean\firebase\app-escuela-c3504-firebase-adminsdk-fbsvc-27b9fd6d46.json')
 firebase_admin.initialize_app(cred)
 
 app = Flask(__name__)
@@ -11,9 +11,17 @@ app = Flask(__name__)
 @app.route('/api/lecciones', methods=['GET'])
 def get_lecciones():
     db = firestore.client()
-    lecciones_ref = db.collection('lecciones')
+    lecciones_ref = db.collection('lecciones')  # Consulta la colección 'lecciones'
     lecciones = lecciones_ref.stream()
     data = [{"id": leccion.id, "contenido": leccion.to_dict()} for leccion in lecciones]
+    return jsonify(data)
+
+@app.route('/api/Lecciones', methods=['GET'])
+def get_Lecciones():
+    db = firestore.client()
+    Lecciones_ref = db.collection('Lecciones')  # Consulta la colección 'Lecciones'
+    Lecciones = Lecciones_ref.stream()
+    data = [{"id": leccion.id, "contenido": leccion.to_dict()} for leccion in Lecciones]
     return jsonify(data)
 
 if __name__ == '__main__':
